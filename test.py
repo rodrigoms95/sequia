b3e846a0d9415f870e31ceaddcb11b8899308666
Rodrigo <rodrigoms95@gmail.com>
2021-06-01 12:36:16 -0500

    prueba ecwmf api

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-import numpy as np
+#!/usr/bin/env python
+from ecmwfapi import ECMWFDataServer
 
-print("hola")
+# To run this example, you need an API key
+# available from https://api.ecmwf.int/v1/key/
+
+server = ECMWFDataServer()
+server.retrieve({
+    'origin'    : "ecmf",
+    'levtype'   : "sfc",
+    'number'    : "1",
+    'expver'    : "prod",
+    'dataset'   : "tigge",
+    'step'      : "0/6/12/18",
+    'area'      : "70/-130/30/-60",
+    'grid'      : "2/2",
+    'param'     : "167",
+    'time'      : "00/12",
+    'date'      : "2014-11-01",
+    'type'      : "pf",
+    'class'     : "ti",
+    'target'    : "tigge_2014-11-01_0012.grib"
+})
