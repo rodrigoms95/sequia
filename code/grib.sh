09162198e6be71676a9e3ee7bbef7dc20f5b20f4
rodrigoms95 <rodrigoms95@gmail.com>
2022-01-19 20:35:07 -0600

    vectorfield

--- code/grib.sh
+++ code/grib.sh
@@ -1,13 +1,16 @@
 INI="1979"
 FIN="1990"
-VAR="u"
+VAR="v"
 LEVEL="500"
 REGION="mexico"
 
-IN='../data/ERA5/'$REGION'_'$VAR'_'$LEVEL'_'$INI'_'$FIN'_daily.grib'
-MID='../data/ERA5/'$REGION'_'$VAR'_'$LEVEL'_'$INI'_'$FIN'_daily_leap.grib'
-OUT='../data/ERA5/'$REGION'_'$VAR'_'$LEVEL'_'$INI'_'$FIN'_daily_mean.grib'
+IN='../data/ERA5/'$REGION'_'$VAR'_'$LEVEL'_'$INI'_'$FIN'_hourly.grib'
+MID='../data/ERA5/'$REGION'_'$VAR'_'$LEVEL'_'$INI'_'$FIN'_hourly_leap.grib'
+OUT_1='../data/ERA5/'$REGION'_'$VAR'_'$LEVEL'_'$INI'_'$FIN'_daily.grib'
+OUT_2='../data/ERA5/'$REGION'_'$VAR'_'$LEVEL'_'$INI'_'$FIN'_daily_mean.grib'
 
-cdo del29feb $IN $MID
-cdo daymean $MID $OUT
+cdo del29feb $IN  $MID
+cdo  daymean $MID $OUT_1
+cdo ydaymean $MID $OUT_2
+
 rm $MID
